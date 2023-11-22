261a85c01c1c09be7aa05cef02ed4f2dd1f73c54
dbsdud00 <dbsdudsw@icloud.com>
2023-11-22 19:12:52 +0900

    todayDateUpdate

--- study_planner/src/components/Calendar.jsx
+++ study_planner/src/components/Calendar.jsx
@@ -6,47 +6,48 @@ import { useEffect } from "react";
 import { addDate, sortDate } from "../feature/calendarSlice";
 import { setDate } from "../feature/dateSlice";
 import { format } from "date-fns";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Calendar = () => {
-  const nowDate = { value: "2023-11-10" };
+  const navigate = useNavigate();
   const calendar = useSelector((state) => state.calendar);
   const date = useSelector((state) => state.date);
   const dispatch = useDispatch();
-  const exDate = "2023-02-02";
+  const addDateAndNavigate = (goalDate) => {
+    console.log("navigateDate");
+    let result = calendar.filter((cal) => cal.value == goalDate);
+    console.log("result : ", result.length == 0);
+    if (calendar.length == 0 || result.length == 0) {
+      dispatch(addDate(goalDate));
+      dispatch(sortDate());
+      navigate(`/${goalDate}`);
+    } else {
+      console.log("날짜중복");
+    }
+  };
   useEffect(() => {
     console.log("useEffect");
-    dispatch(setDate(format(new Date(), "yyyy-MM-dd")));
-    console.log(date.value);
-  }, []);
-  useEffect(() => {}, [date]);
+    const todayDate = format(new Date(), "yyyy-MM-dd");
+    addDateAndNavigate(todayDate);
+    console.log("useEffect 끝");
+  }, [0]);
   const onChangeValue = (e) => {
     dispatch(setDate(e.target.value));
   };
-  const addDateBtn = () => {
-    console.log("date.value : ", date.value);
-    console.log("calendar : ", calendar.length == 0);
-    let result = calendar.filter((cal) => cal.value == date.value);
-    console.log("result : ", result.length == 0);
-    if (calendar.length == 0 || result.length == 0) {
-      dispatch(addDate(date.value));
-      dispatch(sortDate());
-    } else {
-      console.log("중복날짜");
-    }
-  };
+
   const ClickDate = (e) => {
     console.log(e.target.outerText);
+    navigate(`/${e.target.outerText}`);
     dispatch(setDate(e.target.outerText));
   };
   return (
     <div className="Calendar">
       <input type="date" value={date.value} onChange={onChangeValue} />
-      <Button onClick={addDateBtn}>+</Button>
+      <Button onClick={() => addDateAndNavigate(date.value)}>+</Button>
       <ul className="studyDateList">
         {calendar.map((date, index) => (
           <LiForDay key={index} onClick={ClickDate}>
-            <Link to={`/${date.value}`}>{date.value}</Link>
+            {date.value}
           </LiForDay>
         ))}
       </ul>
--- study_planner/src/index.js
+++ study_planner/src/index.js
@@ -7,11 +7,9 @@ import { store } from "./store.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 // import React from "react";
 // import ReactDOM from "react-dom";
